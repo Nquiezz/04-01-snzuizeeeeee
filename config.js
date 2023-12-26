6af59d7135e316cd746ccdb3bd253146437cce81
Nquiezz <154776430+Nquiezz@users.noreply.github.com>
2023-12-26 18:18:53 +0700

    upload

---
+++ config.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <audio controls>
+        <source src="happy.mp3">
+    </audio>
+</body>
+</html>
